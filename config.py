0b3847246478597f110b5b041b22b51bdb550f69
KO_Paing_Lay <107101116+Kopaing12@users.noreply.github.com>
2023-01-11 12:15:05 +0630

    Update config.py
    Signed-off-by: KO_Paing_Lay <107101116+Kopaing12@users.noreply.github.com>

--- config.py
+++ config.py
@@ -23,6 +23,10 @@ DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://bot1:bot1@cluster0.
 # Your database name from mongoDB
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster12")
 DATABASE_NAME2 = str(os.environ.get("DATABASE_NAME2", "Unlimited2"))
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_filesa')
+
+MAX_B_TN = environ.get("MAX_B_TN", "5")
+USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 # ID of users that can use the bot commands
 AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1113630298").split())
