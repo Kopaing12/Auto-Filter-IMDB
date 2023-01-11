aaa5eeb3bed8252f8059718ada1f447291303b20
KO_Paing_Lay <107101116+Kopaing12@users.noreply.github.com>
2023-01-11 12:21:17 +0630

    Update index.py
    Signed-off-by: KO_Paing_Lay <107101116+Kopaing12@users.noreply.github.com>

--- plugins/index.py
+++ plugins/index.py
@@ -94,6 +94,7 @@ async def send_for_index(bot, message):
         reply_markup = InlineKeyboardMarkup(buttons)
         return await message.reply(
             f'Do you Want To Index This Channel/ Group ?\n\nChat ID/ Username: <code>{chat_id}</code>\nLast Message ID: <code>{last_msg_id}</code>',
+            parse_mode=enums.ParseMode.HTML,
             reply_markup=reply_markup)
 
     if type(chat_id) is int:
