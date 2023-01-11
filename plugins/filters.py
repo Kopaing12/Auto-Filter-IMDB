a5bfbc2ab1e8da3520b0300cf8d1612fdd327fa0
KO_Paing_Lay <107101116+Kopaing12@users.noreply.github.com>
2023-01-11 12:33:44 +0630

    Update filters.py
    Signed-off-by: KO_Paing_Lay <107101116+Kopaing12@users.noreply.github.com>

--- plugins/filters.py
+++ plugins/filters.py
@@ -458,7 +458,7 @@ async def give_filter(client, message):
             )
 
 @Client.on_callback_query()
-async def cb_handler(client: Bot, message):
+async def cb_handler(client: Bot, message: CallbackQuery):
     clicked = message.from_user.id
     typed = message.reply_to_message.id if message.reply_to_message else message.id
     
